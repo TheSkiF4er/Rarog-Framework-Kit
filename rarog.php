fd5f6a38befe19a6faa3d9b0e4e413bf9d1394fb
TheSkiF4er <101918317+TheSkiF4er@users.noreply.github.com>
2022-03-21 15:10:28 +1000

    Update rarog.php

--- rarog.php
+++ rarog.php
@@ -25,3 +25,15 @@ function AntiFakeAppeals ($p1 = '0,0,0', $p2 = '3.5') {
  $_SESSION["arr_time"][$min_index]=$time;
 };
 ?>
+
+function Truncation ($p1, $p2) {
+ $str = substr($p1, 0, $p2-2);
+ $words = explode(" ", $p1);
+ array_splice($words,-1);
+ $last = array_pop($words);
+ for ($i=1; $i<strlen($last); $i++) {
+  if (preg_match('/\W$/',substr($last,-1,1))) $last = substr($last,0,strlen($last)-1);
+  else break;
+ }
+ return implode(" ", $words).' '.$last.'...';
+};
