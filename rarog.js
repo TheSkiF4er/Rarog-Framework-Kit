4499d5d2166034fc0e1720907cec521c6496142f
TheSkiF4er <101918317+TheSkiF4er@users.noreply.github.com>
2022-03-22 13:16:21 +1000

    Update rarog.js

--- rarog.js
+++ rarog.js
@@ -14,25 +14,23 @@
 ============================================================================
 */
 function clock() {
-var d = new Date();
-var day = d.getDate();
-var hours = d.getHours();
-var minutes = d.getMinutes();
-var seconds = d.getSeconds();
+ var d = new Date();
+ var day = d.getDate();
+ var hours = d.getHours();
+ var minutes = d.getMinutes();
+ var seconds = d.getSeconds();
 
-month=new Array("января", "февраля", "марта", "апреля", "мая", "июня",
-"июля", "августа", "сентября", "октября", "ноября", "декабря");
-days=new Array("Воскресенье", "Понедельник", "Вторник", "Среда",
-"Четверг", "Пятница", "Суббота");
+ month=new Array("января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря");
+ days=new Array("Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота");
 
-if (day <= 9) day = "0" + day;
-if (hours <= 9) hours = "0" + hours;
-if (minutes <= 9) minutes = "0" + minutes;
-if (seconds <= 9) seconds = "0" + seconds;
+ if (day <= 9) day = "0" + day;
+ if (hours <= 9) hours = "0" + hours;
+ if (minutes <= 9) minutes = "0" + minutes;
+ if (seconds <= 9) seconds = "0" + seconds;
 
-date_date = day + " " + month[d.getMonth()] + " " + d.getFullYear() + " г. (" +
-days[d.getDay()] + ")";
-date_time = hours + ":" + minutes + ":" + seconds;
+ date_date = day + " " + month[d.getMonth()] + " " + d.getFullYear() + " г. (" +
+ days[d.getDay()] + ")";
+ date_time = hours + ":" + minutes + ":" + seconds;
 
 if (document.layers) {
  document.layers.date.document.write(date_time);
@@ -46,4 +44,3 @@ else {
 }
  setTimeout("clock()", 1000);
 }
-clock();
