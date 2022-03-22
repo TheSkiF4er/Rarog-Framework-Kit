f8e07e1435e3f65a6f02c7fbdc62b4742b02f4b4
TheSkiF4er <101918317+TheSkiF4er@users.noreply.github.com>
2022-03-22 13:28:47 +1000

    Update rarog.js

--- rarog.js
+++ rarog.js
@@ -19,28 +19,23 @@ function clock() {
  var hours = d.getHours();
  var minutes = d.getMinutes();
  var seconds = d.getSeconds();
-
  month=new Array("января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря");
  days=new Array("Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота");
-
  if (day <= 9) day = "0" + day;
  if (hours <= 9) hours = "0" + hours;
  if (minutes <= 9) minutes = "0" + minutes;
  if (seconds <= 9) seconds = "0" + seconds;
-
  date_date = day + " " + month[d.getMonth()] + " " + d.getFullYear() + " г. (" +
  days[d.getDay()] + ")";
  date_time = hours + ":" + minutes + ":" + seconds;
-
-if (document.layers) {
- document.layers.date.document.write(date_time);
- document.layers.date.document.close();
- document.layers.time.document.write(date_time);
- document.layers.time.document.close();
-}
-else {
- document.getElementById("date").innerHTML = date_date;
- document.getElementById("time").innerHTML = date_time;
-}
+ if (document.layers) {
+  document.layers.date.document.write(date_time);
+  document.layers.date.document.close();
+  document.layers.time.document.write(date_time);
+  document.layers.time.document.close();
+ } else {
+  document.getElementById("date").innerHTML = date_date;
+  document.getElementById("time").innerHTML = date_time;
+ }
  setTimeout("clock()", 1000);
 }
